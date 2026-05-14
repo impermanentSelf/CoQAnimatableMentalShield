03181146b067eb9d178d17b9adde5f79d2bfed83
oliviao <oliviajolsen14@gmail.com>
2026-05-14 10:49:40 -0500

    added support for the nano-neuro animator

--- animatepatch.cs
+++ animatepatch.cs
@@ -8,7 +8,7 @@ using XRL.World.Parts;
 
 [HarmonyDebug]
 public static class AnimatePatch{
-    public static bool MyAnimate(GameObject frankenObject, GameObject Actor=null, GameObject Using=null){
+    public static bool MyAnimatorSpray(GameObject frankenObject, GameObject Actor=null, GameObject Using=null){
         if (frankenObject.HasPart("MentalShield")){
             frankenObject.RemovePart("MentalShield");
             frankenObject.RequirePart<Sapient>();
@@ -16,12 +16,33 @@ public static class AnimatePatch{
             frankenObject.PlayWorldSound("Sounds/Interact/sfx_interact_sentience_imbue");
             Popup.Show("You grant " + frankenObject.t() + " the gift of Sapience.");
             CombatJuice.playPrefabAnimation(frankenObject, "Abilities/AbilityVFXAnimated", frankenObject.ID, frankenObject.Render.Tile + ";" + frankenObject.Render.GetTileForegroundColor() + ";" + frankenObject.Render.getDetailColor());
+            AnimateEvent.Send(Actor, frankenObject, Using);
             Using?.Destroy();
 
             return true;
         }
+
         Popup.ShowFail("You can't animate an object that already has a brain.");
         return false;
+    }
+
+    public static void MyNanoAnimator(GameObject frankenObject, InventoryActionEvent E=null, GameObject Using=null){
+        if (frankenObject.HasPart("MentalShield")){
+            frankenObject.RemovePart("MentalShield");
+            frankenObject.RequirePart<Sapient>();
+
+            E.Actor.UseEnergy(1000, "Item Animate");
+            frankenObject.PlayWorldSound("Sounds/Interact/sfx_interact_sentience_imbue");
+            if (E.Actor.IsPlayer())
+            {
+                Popup.Show("You grant " + frankenObject.t() + " the gift of Sapience.");
+            }
+
+            AnimateEvent.Send(E.Actor, frankenObject, Using);
+            E.RequestInterfaceExit();
+        } else{
+            Popup.ShowFail("You can't animate an object that already has a brain.");
+        }
     }
 
     /*[HarmonyPatch(typeof(GameObject),nameof(GameObject.HasTagOrProperty))]
@@ -30,27 +51,60 @@ public static class AnimatePatch{
             __result = (Name == "Animatable" && __instance.HasPart("MentalShield"));
         }
     }*/
+
     [HarmonyPatch(typeof(AnimatorSpray),nameof(AnimatorSpray.HandleEvent))]
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator){
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> SprayTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator){
         CodeMatcher codeMatcher = new CodeMatcher(instructions, generator);
 
-        codeMatcher.MatchStartForward(
+        int start = codeMatcher.MatchStartForward(
             new CodeMatch(OpCodes.Ldstr, "You can't animate an object that already has a brain.")
-        ).RemoveInstructions(6); //remove popup dialog and loading a false
+        ).Pos;
+        int end = codeMatcher.MatchStartForward(
+            new CodeMatch(CodeInstruction.Call(typeof(Popup),nameof(Popup.ShowFail)))
+        ).Pos;
+        codeMatcher.RemoveInstructionsInRange(start, end + 1); // remove popup dialog and loading a false
 
-        int pos = codeMatcher.Pos; //save our current position
         LocalBuilder gameObject = codeMatcher.MatchStartBackwards(
             new CodeMatch(OpCodes.Ldloc_S)
-        ).Instruction.operand as LocalBuilder; //previous ldloc.s used the index for the target GameObject
-        codeMatcher.Start().Advance(pos); //return to previous location
+        ).Instruction.operand as LocalBuilder; // previous ldloc.s used the index for the target GameObject
+        codeMatcher.Start().Advance(start); // return to previous location
 
         codeMatcher.Insert(
             new CodeInstruction(OpCodes.Ldloc_S, gameObject),
             new CodeInstruction(OpCodes.Ldarg_1), // Event argument
-            CodeInstruction.LoadField(typeof(InventoryActionEvent), "Actor"), //should be E.Actor
+            CodeInstruction.LoadField(typeof(InventoryActionEvent), "Actor"), // should be E.Actor
             new CodeInstruction(OpCodes.Ldarg_0), // this
-            CodeInstruction.LoadField(typeof(IPart), "_ParentObject"), // this._ParentObject()
-            CodeInstruction.Call(typeof(AnimatePatch), nameof(AnimatePatch.MyAnimate)) // AnimateObject.Animate(gameObject, E.Actor, ParentObject);
+            CodeInstruction.LoadField(typeof(IPart), "_ParentObject"), // this._ParentObject
+            CodeInstruction.Call(typeof(AnimatePatch), nameof(AnimatePatch.MyAnimatorSpray)) // AnimateObject.Animate(gameObject, E.Actor, ParentObject);
+        );
+        return codeMatcher.InstructionEnumeration();
+    }
+
+    [HarmonyPatch(typeof(AnimateObject),nameof(AnimateObject.HandleEvent), typeof(InventoryActionEvent))]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> NanoTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator){
+        CodeMatcher codeMatcher = new CodeMatcher(instructions, generator);
+
+        int start = codeMatcher.MatchStartForward(
+            new CodeMatch(OpCodes.Ldstr, "You can't animate an object that already has a brain.")
+        ).Pos;
+        int end = codeMatcher.MatchStartForward(
+            new CodeMatch(CodeInstruction.Call(typeof(Popup),nameof(Popup.Show)))
+        ).Pos;
+        codeMatcher.RemoveInstructionsInRange(start, end); // remove popup dialog
+
+        LocalBuilder gameObject = codeMatcher.MatchStartBackwards(
+            new CodeMatch(OpCodes.Ldloc_S)
+        ).Instruction.operand as LocalBuilder; // previous ldloc.s used the index for the target GameObject
+        codeMatcher.Start().Advance(start); // return to start location
+
+        codeMatcher.Insert(
+            new CodeInstruction(OpCodes.Ldloc_S, gameObject),
+            new CodeInstruction(OpCodes.Ldarg_1), // Event argument
+            new CodeInstruction(OpCodes.Ldarg_0), // this
+            CodeInstruction.LoadField(typeof(IPart), "_ParentObject"), // this._ParentObject
+            CodeInstruction.Call(typeof(AnimatePatch), nameof(AnimatePatch.MyNanoAnimator)) // AnimateObject.Animate(gameObject, E.Actor, ParentObject);
         );
         return codeMatcher.InstructionEnumeration();
     }
