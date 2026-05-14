a2fd9bae24fe46466139915ef3f61f379b9d63fc
oliviao <oliviajolsen14@gmail.com>
2026-05-14 14:51:08 -0500

    prepared for steam workshop upload, and gave sapient creatures a bonus to their mental attributes

--- sapient.cs
+++ sapient.cs
@@ -5,6 +5,10 @@ using XRL.World;
 [Serializable]
 public class Sapient : IPart
 {
+    int WillpowerAmount;
+    int IntelligenceAmount;
+    int EgoAmount;
+
     public override bool SameAs(IPart p)    {
         return false;
     }
@@ -15,6 +19,17 @@ public class Sapient : IPart
         }
         return true;
     }
+
+    public override void Initialize(){
+        WillpowerAmount = Math.Min(4, Math.Max(0, 16 - ParentObject.Stat("Willpower",Default:4)));
+        IntelligenceAmount = Math.Min(4, Math.Max(0, 16 - ParentObject.Stat("Intelligence",Default:4)));
+        EgoAmount = Math.Min(4, Math.Max(0, 16 - ParentObject.Stat("Ego",Default:4)));
+
+        base.StatShifter.SetStatShift(ParentObject, "Willpower", WillpowerAmount, baseValue:true);
+        base.StatShifter.SetStatShift(ParentObject, "Intelligence", IntelligenceAmount, baseValue:true);
+        base.StatShifter.SetStatShift(ParentObject, "Ego", EgoAmount, baseValue:true);
+    }
+
     public override bool HandleEvent(GetDisplayNameEvent E)    {
         if (!E.Object.HasProperName && !E.Object.HasTagOrProperty("NoAnimatedNamePrefix"))        {
             E.AddAdjective("sapient", 5);
